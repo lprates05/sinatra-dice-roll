d507e001b1d52aa0ca60ff2e60888c4542e88836
lprates05 <lorenapratesdl@outlook.com>
2025-04-09 15:30:46 +0000

    dice

--- dice.rb
+++ dice.rb
@@ -1,9 +1,15 @@
 require "sinatra"
+require "sinatra/reloader"
+set :environment, :development
 
 get("/") do
   "Hello World"
-end
+end 
 
 get("/zebra") do
   "We must add a route for each path we want to support"
+end 
+
+get("/giraffe") do
+  "Hopefully this shows up without having to restart the server 🤞🏾"
 end
