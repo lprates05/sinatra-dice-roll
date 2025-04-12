4f28b8625237f421be9e58123ad897a4c4536ad0
lprates05 <lorenapratesdl@outlook.com>
2025-04-12 18:12:57 +0000

    dice

--- dice.rb
+++ dice.rb
@@ -1,15 +1,55 @@
 require "sinatra"
 require "sinatra/reloader"
+
+require "better_errors"
+require "binding_of_caller"
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
 set :environment, :development
 
 get("/") do
-  "Hello World"
+  erb(:elephant)
+end
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
+
+  erb(:two_six, {:layout=> :wrappper})
 end 
 
-get("/zebra") do
-  "We must add a route for each path we want to support"
+get ("/dice/2/10") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
+
+  erb(:two_ten)
 end 
 
-get("/giraffe") do
-  "Hopefully this shows up without having to restart the server 🤞🏾"
+get ("/dice/1/20") do
+  first_die = rand (1..20)
+  @outcome = "You rolled a #{first_die}"
+
+  erb(:one_twenty)
+end
+
+get ("/dice/5/4") do
+  first_die = rand (1..4)
+  second_die = rand (1..4)
+  third_die = rand (1..4)
+  fourth_die = rand (1..4)
+  fifth_die = rand (1..4)
+  sum = first_die + second_die + third_die + fourth_die + fifth_die
+
+  @outcome = "You rolled #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and #{fifth_die} for a total of #{sum}"
+
+  erb(:five_four)
 end
